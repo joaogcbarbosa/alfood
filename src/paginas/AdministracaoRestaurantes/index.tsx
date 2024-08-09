dd0d3dd5d9662bfa2e96d92b10d3b4bfcec87d0f
joaogcbarbosa <joao.barbosa@rioenergy.com.br>
2024-08-09 14:55:54 -0300

    Chore: admin page

---
+++ src/paginas/AdministracaoRestaurantes/index.tsx
@@ -0,0 +1,39 @@
+import Table from '@mui/material/Table';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { TableBody } from '@mui/material';
+import IRestaurante from '../../interfaces/IRestaurante';
+
+const AdministracaoRestaurantes = () => {
+
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+
+    useEffect(() => {
+        axios.get<IRestaurante[]>("http://localhost:8000/api/v2/restaurantes/")
+            .then(response => setRestaurantes(response.data))
+            .catch(error => console.log(error))
+    })
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableBody>
+                    {restaurantes.map(restaurante => (
+                        <TableRow key={restaurante.id}>
+                            <TableCell>
+                                {restaurante.nome}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default AdministracaoRestaurantes
